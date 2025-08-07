f035b8adeb85a7db357f7e24c435f82e5667d863
thushan-123 <thushanmadusanka456@gmail.com>
2025-08-07 19:59:33 +0530

    inputs stdin or file

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <ctype.h>
 
 
-int main (int args, char *argv[]) {
+int main (int argc, char *argv[]) {
 
     FILE *file;
 
@@ -11,5 +11,13 @@ int main (int args, char *argv[]) {
     int lines = 0;
     int word = 0;
     int characters = 0;
+
+    int word_in = 0 ;  // boolen vlue
+
+    if(argc <2){
+        file = stdin;
+    }else{
+        file = fopen(argv[1], 'r');
+    }
     return 0;
 }
